4d76aec2419de8ce63ee3ddd4184beff3022e9dc
storm37000 <6331913+storm37000@users.noreply.github.com>
2021-12-12 10:28:09 -0500

    add all the build configs

--- build.ps1
+++ build.ps1
@@ -7,8 +7,14 @@ if($env:APPVEYOR_BUILD_WORKER_IMAGE -eq "Visual Studio 2019"){
 	Invoke-WebRequest -Uri "https://github.com/premake/premake-core/releases/download/v5.0.0-beta1/premake-5.0.0-beta1-windows.zip" -OutFile "premake-5.0.0-beta1-windows.zip"
 	Expand-Archive premake-5.0.0-beta1-windows.zip ./
 	premake5.exe --os=windows vs2019
-	msbuild ".\projects\windows\gmsv_antifreeze.sln" /property:Configuration=Release /p:Platform="Win32" /verbosity:normal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
-	msbuild ".\projects\windows\gmsv_antifreeze.sln" /property:Configuration=Release64 /p:Platform="x64" /verbosity:normal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
+	msbuild ".\projects\windows\gmsv_antifreeze.sln" /property:Configuration=release32_sse /p:Platform="Win32" /verbosity:normal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
+	msbuild ".\projects\windows\gmsv_antifreeze.sln" /property:Configuration=release32_avx128 /p:Platform="Win32" /verbosity:normal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
+	msbuild ".\projects\windows\gmsv_antifreeze.sln" /property:Configuration=release32_avx256 /p:Platform="Win32" /verbosity:normal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
+	msbuild ".\projects\windows\gmsv_antifreeze.sln" /property:Configuration=release32_avx512 /p:Platform="Win32" /verbosity:normal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
+	msbuild ".\projects\windows\gmsv_antifreeze.sln" /property:Configuration=release64_sse /p:Platform="x64" /verbosity:normal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
+	msbuild ".\projects\windows\gmsv_antifreeze.sln" /property:Configuration=release64_avx128 /p:Platform="x64" /verbosity:normal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
+	msbuild ".\projects\windows\gmsv_antifreeze.sln" /property:Configuration=release64_avx256 /p:Platform="x64" /verbosity:normal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
+	msbuild ".\projects\windows\gmsv_antifreeze.sln" /property:Configuration=release64_avx512 /p:Platform="x64" /verbosity:normal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
 }else{
 	$contentold = Get-Content -Path './src/main.cpp'
 	echo($contentold.Replace('LUA->PushNumber(0);//replace_build_number_here_automatic!', "LUA->PushNumber($env:APPVEYOR_BUILD_NUMBER);"))
@@ -21,6 +27,12 @@ if($env:APPVEYOR_BUILD_WORKER_IMAGE -eq "Visual Studio 2019"){
 	tar –xzf premake-5.0.0-beta1-linux.tar.gz
 	premake5 --os=linux gmake2
 	cd ./projects/linux
-	make config=release
-	make config=release64
+	make config=release32_sse
+	make config=release32_avx128
+	make config=release32_avx256
+	make config=release32_avx512
+	make config=release64_sse
+	make config=release64_avx128
+	make config=release64_avx256
+	make config=release64_avx512
 }
